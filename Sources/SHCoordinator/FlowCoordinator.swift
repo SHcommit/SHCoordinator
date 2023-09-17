bff3503ebfc0992770f91deaea5ecf200ad27a98
shcommit <happysh_s2@naver.com>
2023-09-17 23:50:29 +0900

    [Feat] Changed protocol specific func optional keyword

--- Sources/SHCoordinator/FlowCoordinator.swift
+++ Sources/SHCoordinator/FlowCoordinator.swift
@@ -8,12 +8,12 @@
 #if os(iOS)
 import UIKit
 
-public protocol FlowCoordinator: AnyObject {
+@objc public protocol FlowCoordinator: AnyObject {
   // MARK: - Properties
   var parent: FlowCoordinator! { get set }
   var child: [FlowCoordinator] { get set }
   var presenter: UINavigationController { get }
-  var viewController: UIViewController! { get set }
+  @objc optional weak var viewController: UIViewController? { get set }
   
   // MARK: - Helpers
   func start()
@@ -22,21 +22,19 @@ public protocol FlowCoordinator: AnyObject {
 extension FlowCoordinator {
   
   /// If a ChildCoordinator needs to be deallocated when a viewController is about to disappear,
-  /// it is important to call the **finish** method on the weakly held coordinator in the viewWillDisappear(_:) or viewDidDisappear(_:) method of the viewController to prevent memory leaks.
+  /// it is important to call the **finish** method on the weakly held coordinator in the deinit of the viewController to prevent memory leaks.
   ///
   /// In the child coordinator, the parent coordinator is holding the child coordinator instance as a chid array instance.
   /// That's why instances can be maintained so that the child Coordinator can switch between multiple screens.
   /// When it is deleted, it must be removed from the parent holding its object.
   ///
-  /// Here is an example in English:
+  /// Here is an example:
   ///
   /// ```
   /// class MyViewController: UIViewController {
   ///   weak var coordinator: ChildCoordinator?
   ///
-  ///   override func viewWillDisappear(_ animated: Bool) {
-  ///     super.viewWillDisappear(animated)
-  ///     //
+  ///   deinit {
   ///     coordinator?.finish()
   ///   }
   /// }

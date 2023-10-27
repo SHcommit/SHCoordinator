bf52038a63a893ddce060c2a1f268342a900e62a
shcommit <happysh_s2@naver.com>
2023-10-27 20:28:26 +0900

    [Feat] SubCoordinator에 선언했던 SubViewController 분리

--- SHCoordinatorExample/Source/Feature/SubViewController.swift
+++ SHCoordinatorExample/Source/Feature/SubViewController.swift
@@ -5,4 +5,28 @@
 //  Created by 양승현 on 2023/09/18.
 //
 
-import Foundation
+import UIKit
+
+final class SubViewController: UIViewController {
+  weak var coordinator: SubCoordinatorDelegate?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .yellow
+    setLabel()
+  }
+  
+  private func setLabel() {
+    let screenWidth = Int(UIScreen.main.bounds.width)
+    let width = 200
+    let rect = CGRect(x: (screenWidth-width)/2, y: 300, width: width, height: 100)
+    let lb = UILabel(frame: rect)
+    lb.textAlignment = .center
+    lb.text = "Sub View Controller"
+    view.addSubview(lb)
+  }
+  
+  deinit {
+    print("\(Self.self) deinit")
+  }
+}

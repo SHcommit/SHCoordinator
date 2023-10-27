229558d0feb8687ccbd2b7f54518c5a90769dc34
shcommit <happysh_s2@naver.com>
2023-10-27 20:26:37 +0900

    [Feat] protocol public으로 선언

--- FlowCoordinatorDelegate.swift
+++ FlowCoordinatorDelegate.swift
@@ -5,9 +5,10 @@
 //  Created by 양승현 on 10/27/23.
 //
 
+#if os(iOS)
 import Foundation
 
-public protocol FlowCoordinatorDelegate {
+public protocol FlowCoordinatorDelegate: AnyObject {
   /// 현재 보여지는 coordinator의 인스턴스를 제거하기 위해선 이 함수를 호출해야 합니다.
   /// 특정 흐름에 의해 뒤로가야하는 경우일떄, navigationController.popViewController(animated:)를 호출하지 않은 경우 이 함수를 호출해야 합니다.
   func finish(withAnimated animated: Bool)
@@ -16,3 +17,4 @@ public protocol FlowCoordinatorDelegate {
   /// 만약 baseViewController에서 네비게이션 백 버튼을 추가해둘 경우 이 함수를 호출해야합니다.
   func finish()
 }
+#endif
